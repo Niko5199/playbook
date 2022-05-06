0c696472bba8a87dcf99e4e25c0ccf28042c0440
JARP <paredesjosue77@gmail.com>
2022-05-05 21:27:22 -0500

    Terminando Creación de proyectos de JS

---
+++ weekly_mission_3/my_launchx_app/index.js
@@ -0,0 +1,11 @@
+const MissionCommander = require('./app/MissionCommander');
+
+
+const carlo = new MissionCommander('Carlo');
+const josue = new MissionCommander('Josue');
+const cielo = new MissionCommander('Cielo');
+
+
+console.log(carlo);
+console.log(josue);
+console.log(cielo);
